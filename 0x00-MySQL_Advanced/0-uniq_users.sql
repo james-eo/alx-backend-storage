7328666fa70e7156dede9f41939603cba1418d3b
james-eo <jameseokonkwo@gmail.com>
2024-07-11 08:53:45 +0000

    create table users with UNIQUE constraint

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Creates a table users with constraints on the email column
 
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	);
+	name VARCHAR(255)
+);
